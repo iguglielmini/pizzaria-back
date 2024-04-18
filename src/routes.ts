1ee665fe720eebb3143b5a4ccd0f9f1a8edd9fa4
Italo Giovanni Pedroza Guglielmini <italo.guglielmini.3@globalhitss.com.br>
2024-04-18 12:29:40 -0300

    initial project

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.get("/test", (req: Request, res: Response) => {
+  return res.json({ ok: true });
+
+//   throw new Error("Erro ao fazer essa requisição");
+});
+
+export { router };
