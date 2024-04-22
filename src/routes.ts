63a670de4ffd7955659472c68de959f55e90b3db
Italo Giovanni Pedroza Guglielmini <italo.guglielmini.3@globalhitss.com.br>
2024-04-22 10:36:00 -0300

    Feat: category services e controller

--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,10 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/user/createUserController";
 import { AuthUserController } from "./controllers/user/authUserController";
 import { DetailUserController } from "./controllers/user/detailUserController";
+import { CreateCategoryController } from "./controllers/category/createCategoryController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { ListCategoryController } from "./controllers/category/listCategoryController";
 
 const router = Router();
 
@@ -12,5 +14,12 @@ const router = Router();
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+// Category
+router.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle
+);
+router.get("/category", isAuthenticated, new ListCategoryController().handle);
 
 export { router };
